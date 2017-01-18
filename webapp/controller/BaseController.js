85b9ba0028cf2eb78711435b8a53c515c8939892
wozjac <wozjac@zoho.com>
2017-01-18 19:47:00 +0100

    Formatting

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -1,30 +1,30 @@
 /* global sap */
-sap.ui.define([ "sap/ui/core/mvc/Controller" ], function(Controller) {
-	"use strict";
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+  "use strict";
 
-	return Controller.extend("RESOURCE_ROOT.BaseController", {
-		getRouter : function () {
-			return sap.ui.core.UIComponent.getRouterFor(this);
-		},
+  return Controller.extend("RESOURCE_ROOT.BaseController", {
+    getRouter: function () {
+      return sap.ui.core.UIComponent.getRouterFor(this);
+    },
 
-		getModel : function (name) {
-			return this.getView().getModel(name);
-		},
+    getModel: function (name) {
+      return this.getView().getModel(name);
+    },
 
-		setModel : function (model, name) {
-			return this.getView().setModel(model, name);
-		},
+    setModel: function (model, name) {
+      return this.getView().setModel(model, name);
+    },
 
-		getResourceBundle : function () {
-			return this.getOwnerComponent().getModel("i18n").getResourceBundle();
-		},
+    getResourceBundle: function () {
+      return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+    },
 
-		getContentDensityClass : function() {
-			return this.getOwnerComponent().getContentDensityClass();
-		},
+    getContentDensityClass: function () {
+      return this.getOwnerComponent().getContentDensityClass();
+    },
 
-		byId : function(id) {
-			return this.getView().byId(id);
-		}
-    });
+    byId: function (id) {
+      return this.getView().byId(id);
+    }
+  });
 });
